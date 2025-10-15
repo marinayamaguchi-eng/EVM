14784880525c676563379026e24a8e6c1b3c704f
marinayamaguchi-eng <marina_yamaguchi@fsz.co.jp>
2025-10-15 17:30:09 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+// 動作確認用ルート
+app.get('/', (req, res) => {
+  res.send('✅ Smartsheet Webhook サーバーが動いてます！');
+});
+
+// Webhook用ルート
 app.post('/webhook', (req, res) => {
   console.log('📩 Webhook received:', JSON.stringify(req.body, null, 2));
 
+  // Hook チャレンジの確認応答
   if (req.headers['smartsheet-hook-challenge']) {
     res.set('Smartsheet-Hook-Response', req.headers['smartsheet-hook-challenge']);
     return res.sendStatus(200);
