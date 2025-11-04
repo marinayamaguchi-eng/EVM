c097af1120898adb5297c81ca32beca4199ca673
marinayamaguchi-eng <marina_yamaguchi@fsz.co.jp>
2025-11-04 09:28:24 +0900

    Update registerWebhook.js

--- registerWebhook.js
+++ registerWebhook.js
@@ -1,15 +1,15 @@
 const axios = require("axios");
 
 const TOKEN = process.env.SMARTSHEET_ACCESS_TOKEN;  // 個人アクセストークン
-const SHEET_ID = Number(process.env.SOURCE_SHEET_ID);              // 日付マスタのシートID
+const SHEET_ID = Number(process.env.SOURCE_SHEET_ID); // 日付マスタのシートID
 
 async function createWebhook() {
   try {
     const resp = await axios.post(
       "https://api.smartsheet.com/2.0/webhooks",
       {
-        name: "DateMasterWebhook",                        // 好きな名前
-        callbackUrl: "https://evm-zdt5.onrender.com/webhook", // RenderのURL
+        name: "DateMasterWebhook", // 好きな名前
+        callbackUrl: process.env.RENDER_CALLBACK_URL, // RenderのURL
         scope: "sheet",
         scopeObjectId: SHEET_ID,
         events: ["*.*"], // 全イベント受け取る（更新のみなら ["*"] のままでOK）
@@ -29,6 +29,7 @@ async function createWebhook() {
 }
 
 createWebhook();
+
 
 
 
